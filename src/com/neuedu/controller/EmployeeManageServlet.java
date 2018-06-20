5b1688329aeb2e97d6196a10679ceb271751b821
lenovo <lenovo@Lenovo-PC>
2018-06-20 15:19:38 +0800

    登录相关的EmployeeManageService和EmployeeServlet

---
+++ src/com/neuedu/controller/EmployeeManageServlet.java
@@ -0,0 +1,148 @@
+package com.neuedu.controller;
+import com.neuedu.model.po.Employee;
+import com.neuedu.model.service.*;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmployeeManageServlet
+ */
+public class EmployeeManageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeeManageServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		
+		String action = request.getParameter("action");
+		if("login".equals(action)){
+			login(request,response);
+		}else if("register".equals(action)){
+			register(request,response);
+		}
+	}
+
+	private void login(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		String account = request.getParameter("account");
+		String password = request.getParameter("password");
+						
+		//数据库查询结果
+		Employee e = EmployeeManageService.getInstance().login(account);
+		
+		if(e==null){
+			//账户不存在
+			request.setAttribute("message", "该账户不存在");
+			try {
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			} catch (ServletException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else if(!e.getEmployeePassword().equals(password)){
+			//密码不正确
+			request.setAttribute("message", "密码错误");
+			try {
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			} catch (ServletException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}else{
+			//账户正确
+			int center = e.getCenterType();
+			request.getSession().setAttribute("employee",e);
+			switch(center){
+				case 1:
+					//客户服务中心
+					try {
+						response.sendRedirect(request.getContextPath()+"");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+				case 2:
+					//调度中心
+					try {
+						response.sendRedirect(request.getContextPath()+"/.jsp");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+				case 3:
+					//分站中心
+					try {
+						response.sendRedirect(request.getContextPath()+"/.jsp");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+				case 4:
+					//库房中心
+					try {
+						response.sendRedirect(request.getContextPath()+"/.jsp");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+				case 5:
+					//配送中心
+					try {
+						response.sendRedirect(request.getContextPath()+"/.jsp");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+				case 6:
+					//财务中心
+					try {
+						response.sendRedirect(request.getContextPath()+"/.jsp");
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					break;
+			}
+		}
+	}
+	
+	private void register(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/neuedu/model/service/EmployeeManageService.java
@@ -0,0 +1,28 @@
+package com.neuedu.model.service;
+
+import java.sql.Connection;
+
+import com.neuedu.model.dao.EmployeeDAO;
+import com.neuedu.model.dao.EmployeeDAOImp;
+import com.neuedu.model.po.Employee;
+import com.neuedu.utils.DBUtil;
+
+public class EmployeeManageService {
+	
+	//单例模式
+	private static EmployeeManageService service = new EmployeeManageService();
+	private EmployeeManageService(){}
+	public static EmployeeManageService getInstance(){
+		return service;
+	}
+	
+	//查询账户信息
+	public Employee login(String account) {
+		Connection conn = DBUtil.getConn();
+		EmployeeDAO dao = new EmployeeDAOImp(conn);
+		Employee e = dao.login(account);
+		return e;
+	}
+	
+	
+}
