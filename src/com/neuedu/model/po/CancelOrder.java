a58bbda9c63ef0d99d825b57e26c3d100df6fdc9
Ayanami <Ayanami@zhou>
2018-06-22 15:52:23 +0800

    为cancelorder实体增加operator类以及operatordate方便封装数据 修改newId 为NewOrder实体，可以直接通过这个引用获得新订订单相关信息

--- src/com/neuedu/model/po/CancelOrder.java
+++ src/com/neuedu/model/po/CancelOrder.java
@@ -4,20 +4,25 @@ import java.util.Date;
 
 public class CancelOrder {
 	private int cancelOrderId;
-	private int newOrderId;
+	private NewOrder newOrder;
+	private int employeeId;
 	private String cancelReason;
 	private Date cancelDate;
+	//虽然订单中不存在删改，但是存在修改新订订单的行为，所以封装操作者名字和时间用于数据维护
+	private String operator;
+	private Date operatorDate;
 	public int getCancelOrderId() {
 		return cancelOrderId;
 	}
 	public void setCancelOrderId(int cancelOrderId) {
 		this.cancelOrderId = cancelOrderId;
 	}
-	public int getNewOrderId() {
-		return newOrderId;
+	
+	public NewOrder getNewOrder() {
+		return newOrder;
 	}
-	public void setNewOrderId(int newOrderId) {
-		this.newOrderId = newOrderId;
+	public void setNewOrder(NewOrder newOrder) {
+		this.newOrder = newOrder;
 	}
 	public String getCancelReason() {
 		return cancelReason;
@@ -31,5 +36,24 @@ public class CancelOrder {
 	public void setCancelDate(Date cancelDate) {
 		this.cancelDate = cancelDate;
 	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getOperator() {
+		return operator;
+	}
+	public void setOperator(String operator) {
+		this.operator = operator;
+	}
+	public Date getOperatorDate() {
+		return operatorDate;
+	}
+	public void setOperatorDate(Date operatorDate) {
+		this.operatorDate = operatorDate;
+	}
+	
 	
 }
