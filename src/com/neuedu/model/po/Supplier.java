40de741ea511e6e4f33a64a7a88a6300ed661a0d
Administrator <Administrator@PC-20171022NXER>
2018-06-27 20:06:10 +0800

    增加了客户和供应商的增删改查

--- src/com/neuedu/model/po/Supplier.java
+++ src/com/neuedu/model/po/Supplier.java
@@ -2,6 +2,132 @@ package com.neuedu.model.po;
 
 public class Supplier {
 	private int supId;
+	private String supName;
+	private String supAddress;
+	private String supLinkman;
+	private String supPhone;
+	private String supBankName;
+	private int supBankAccount;
+	private int supPostcode;
+	private String supLegalPerson;
+	private String supRemark;
+	private int supFax;
+	private int status;
+	private String operate;
+	private String operateDate;
+	
+	public int getSupFax() {
+		return supFax;
+	}
+
+	public void setSupFax(int supFax) {
+		this.supFax = supFax;
+	}
+
+
+	
+
+	public String getSupName() {
+		return supName;
+	}
+
+	public void setSupName(String supName) {
+		this.supName = supName;
+	}
+
+
+
+	public String getSupAddress() {
+		return supAddress;
+	}
+
+	public void setSupAddress(String supAddress) {
+		this.supAddress = supAddress;
+	}
+
+	public String getSupLinkman() {
+		return supLinkman;
+	}
+
+	public void setSupLinkman(String supLinkman) {
+		this.supLinkman = supLinkman;
+	}
+
+	public String getSupPhone() {
+		return supPhone;
+	}
+
+	public void setSupPhone(String supPhone) {
+		this.supPhone = supPhone;
+	}
+
+	public String getSupBankName() {
+		return supBankName;
+	}
+
+	public void setSupBankName(String supBankName) {
+		this.supBankName = supBankName;
+	}
+
+
+
+	public int getSupBankAccount() {
+		return supBankAccount;
+	}
+
+	public void setSupBankAccount(int supBankAccount) {
+		this.supBankAccount = supBankAccount;
+	}
+
+	public int getSupPostcode() {
+		return supPostcode;
+	}
+
+	public void setSupPostcode(int supPostcode) {
+		this.supPostcode = supPostcode;
+	}
+
+	public String getSupLegalPerson() {
+		return supLegalPerson;
+	}
+
+	public void setSupLegalPerson(String supLegalPerson) {
+		this.supLegalPerson = supLegalPerson;
+	}
+
+	public String getSupRemark() {
+		return supRemark;
+	}
+
+	public void setSupRemark(String supRemark) {
+		this.supRemark = supRemark;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getOperate() {
+		return operate;
+	}
+
+	public void setOperate(String operate) {
+		this.operate = operate;
+	}
+
+
+
+	public String getOperateDate() {
+		return operateDate;
+	}
+
+	public void setOperateDate(String operateDate) {
+		this.operateDate = operateDate;
+	}
 
 	public int getSupId() {
 		return supId;
@@ -12,3 +138,17 @@ public class Supplier {
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
