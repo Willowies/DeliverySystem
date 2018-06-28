5f4746b1fcad8e68c480ecf027dfc6a69708cc1f
Ayanami <Ayanami@zhou>
2018-06-29 00:36:31 +0800

    orderservice

--- src/com/neuedu/model/service/CancelOrderService.java
+++ src/com/neuedu/model/service/CancelOrderService.java
@@ -11,17 +11,14 @@ import com.neuedu.model.dao.NewOrderDAOImp;
 import com.neuedu.model.po.CancelOrder;
 import com.neuedu.utils.DBUtil;
 
-
-
 public class CancelOrderService {
 	private CancelOrderService(){
 	}
-	
 	private static CancelOrderService service = new CancelOrderService();
-	
 	public static CancelOrderService getInstance(){
 		return service;
 	}
+	
 	public void cancelOrder(CancelOrder cancelOrder){
 		Connection conn = DBUtil.getConn();
 		CancelOrderDAO canceldao = new CancelOrderDAOImp(conn);
@@ -34,11 +31,12 @@ public class CancelOrderService {
 		Date operatorDate = cancelOrder.getOperatorDate();
 		*/
 		//将新订订单中状态更改为退订状态
-		NewOrderDAO newdao = new NewOrderDAOImp(conn);
+		Connection conn2 = DBUtil.getConn();
+		NewOrderDAO newdao = new NewOrderDAOImp(conn2);
 		String operator = cancelOrder.getOperator();
 		Date operatorDate = cancelOrder.getOperateDate();
 		newdao.setNewOrderState(newOrderId, orderState, operator, operatorDate);
-		DBUtil.closeConn(conn);
+		DBUtil.closeConn(conn2);
 	}
 	public List<CancelOrder> selectCancelOrder(CancelOrder c){
 		Connection conn = DBUtil.getConn();
@@ -47,10 +45,18 @@ public class CancelOrderService {
 		DBUtil.closeConn(conn);
 		return cancelOrders;
 	}
-	public void selectCancelOrderByPage(CancelOrder cancelorder,int pageSize,int pageNUm){
-		
+	public List<CancelOrder> selectCancelOrderByPage(CancelOrder cancelOrder,int pageSize,int pageNum){
+		Connection conn = DBUtil.getConn();
+		CancelOrderDAO canceldao = new CancelOrderDAOImp(conn);
+		List<CancelOrder> cancelOrders = canceldao.selectCancelOrderByPage(cancelOrder,pageSize,pageNum);
+		DBUtil.closeConn(conn);
+		return cancelOrders;
 	}
-	public void selectCancelOrderPageCount(CancelOrder cancelorder,int pageSize,int pageNUm){
-		
+	public int selectCancelOrderPageCount(CancelOrder cancelOrder,int pageSize){
+		Connection conn = DBUtil.getConn();
+		CancelOrderDAO canceldao = new CancelOrderDAOImp(conn);
+		int cancelOrderPageCount = canceldao.selectCancelOrderPageCount(cancelOrder, pageSize);
+		DBUtil.closeConn(conn);
+		return cancelOrderPageCount;
 	}
 }
--- src/com/neuedu/model/service/NewOrderService.java
+++ src/com/neuedu/model/service/NewOrderService.java
@@ -9,7 +9,6 @@ import com.neuedu.model.dao.NewOrderDAOImp;
 import com.neuedu.model.po.NewOrder;
 import com.neuedu.utils.DBUtil;
 
-
 public class NewOrderService {
 	private NewOrderService(){
 	}
@@ -23,33 +22,45 @@ public class NewOrderService {
 		Connection conn = DBUtil.getConn();
 		NewOrderDAO dao = new NewOrderDAOImp(conn);
 		dao.creatNewOrder(newOrder);
+		DBUtil.closeConn(conn);
 	}
 	public void deleteNewOrderById(int newOrderId,String operator,Date operatorDate){
 		Connection conn = DBUtil.getConn();
 		NewOrderDAO dao = new NewOrderDAOImp(conn);
 		dao.deleteNewOrderById(newOrderId, operator, operatorDate);
+		DBUtil.closeConn(conn);
 	}
 	public void setNewOrderState(int orderId,int orderState,String operator,Date operatorDate){
 		Connection conn = DBUtil.getConn();
 		NewOrderDAO dao = new NewOrderDAOImp(conn);
 		dao.setNewOrderState(orderId,orderState,operator,operatorDate);
-		
+		DBUtil.closeConn(conn);
 	}
-	public void updateNewOrder(NewOrder newOrder){}{
-		
+	public void updateNewOrder(NewOrder newOrder){
+		Connection conn = DBUtil.getConn();
+		NewOrderDAO dao = new NewOrderDAOImp(conn);
+		dao.updateNewOrder(newOrder);
+		DBUtil.closeConn(conn);
 	}
 	public List<NewOrder> selectNewOrder(NewOrder newOrder){
 		Connection conn = DBUtil.getConn();
 		NewOrderDAO dao = new NewOrderDAOImp(conn);
-		return dao.selectNewOrder(newOrder);
+		List<NewOrder> newOrders = dao.selectNewOrder(newOrder);
+		DBUtil.closeConn(conn);
+		return newOrders;
 	}
 	public List<NewOrder> selectNewOrderByPage(NewOrder newOrder,int pageSize,int pageNum){
 		Connection conn = DBUtil.getConn();
 		NewOrderDAO dao = new NewOrderDAOImp(conn);
+		List<NewOrder> newOrders = dao.selectNewOrderByPage(newOrder,pageSize,pageNum);
 		DBUtil.closeConn(conn);
-		return dao.selectNewOrderByPage(newOrder,pageSize,pageNum);
+		return newOrders;
 	}
-	public int selectNewOrderPageCount(NewOrder newOrder,int pageSize,int pageNum){
-		return 0;
+	public int selectNewOrderPageCount(NewOrder newOrder,int pageSize){
+		Connection conn = DBUtil.getConn();
+		NewOrderDAO dao = new NewOrderDAOImp(conn);
+		int newOrderPageCount = dao.selectNewOrderPageCount(newOrder, pageSize);
+		DBUtil.closeConn(conn);
+		return newOrderPageCount;
 	}
 }
