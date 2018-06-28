651d6b49765d99b876e247b06a4c29e78de86715
Willowies <z654714845@126.com>
2018-06-28 20:35:49 +0800

    Revert "Revert "跟新客户""
    This reverts commit 426c4c52842f508495ae2ba8f34e7290f4d2aebc.

---
+++ WebContent/js/validateClientIc.js
@@ -0,0 +1,50 @@
+var req;
+function validate(){
+	
+	/*
+	 * XMLHTTPRequest
+	 * 1. 创建对象
+	 * 2. 建立要连接的URL
+	 * 3. 打开到服务器的连接
+	 * 4. 设置回调函数
+	 * 5. 发送请求
+	 */
+	if(window.XMLHttpRequest){
+		//非IE浏览器
+		req = new XMLHttpRequest();
+	}else if(window.ActiveXObject){
+		//IE浏览器
+		req = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	if(req){
+		//打开到服务器的连接
+		req.open("post","clientManageServlet?action=validateIc",false);
+		//设定请求头信息
+		req.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+		//回调函数
+		req.onreadystatechange = updatePage;
+		//发送请求
+		req.send("clientIc="+document.getElementById("clientIc").value);
+	}
+	
+}
+
+function updatePage(){
+	if(req.readyState == 4){
+		if(req.status == 200){
+			var result = req.responseText;
+			if(result == "true"){
+				//重复
+				document.getElementById("res").innerHTML = "已存在";
+			}else{
+				//不重复
+				document.getElementById("res").innerHTML = "可用";
+			}
+		}
+	}
+}
+
+
+
+
+
